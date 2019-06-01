a2e9dfcc75589b721b550c0b03c3acbcabdd058f
Natalia Sturza <Natalia.Sturza@nike.com>
2019-05-31 21:19:40 -0700

    commiting changes

--- test-app/src/main/java/com/mycompany/app/App.java
+++ test-app/src/main/java/com/mycompany/app/App.java
@@ -5,23 +5,20 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-    	try {
-			HttpResponse<JsonNode> jsonResponse = 
-					Unirest.get("https://pokeapi.co/api/v2/pokemon/ditto/")
-					  .header("accept", "application/json")
-					  .asJson();
-			System.out.println( jsonResponse.getBody() );
-		} catch (UnirestException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    try {
+    HttpResponse<JsonNode> jsonResponse =
+    Unirest.post("http://localhost:8080/openidm/managed/user/?_queryId=query-all-ids")
+    .header("X-OpenIDM-Username", "openidm-admin")
+    .header("X-OpenIDM-Password", "openidm-admin")
+    .asJson();
+	System.out.println( jsonResponse.getStatus() );
+	} catch (UnirestException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
     }
 }
